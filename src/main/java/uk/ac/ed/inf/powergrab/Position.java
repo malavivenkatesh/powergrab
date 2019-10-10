09c364a07831cfd3edc4fa541a70849d5cacd1a2
Malavika Venkatesh <s1709617@kjus.inf.ed.ac.uk>
2019-10-10 19:28:30 +0100

    Add Position class and function template

---
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -0,0 +1,18 @@
+package uk.ac.ed.inf.powergrab;
+
+public class Position {
+	public double latitude;
+	public double longitude;
+	
+	public Position(double latitude, double longitude) { 
+	}
+	
+	public Position nextPosition(Direction direction) {
+		return null;
+	}
+	
+	public boolean inPlayArea() {
+		
+		return true;
+	}
+}
