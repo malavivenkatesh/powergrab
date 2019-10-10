aea29ba805f3da366dd63dc3781c18a23a4a4921
Malavika Venkatesh <s1709617@kjus.inf.ed.ac.uk>
2019-10-10 19:29:50 +0100

    Update Position class with constructor and add inPlayArea function

--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -5,6 +5,8 @@ public class Position {
 	public double longitude;
 	
 	public Position(double latitude, double longitude) { 
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 	
 	public Position nextPosition(Direction direction) {
@@ -12,6 +14,15 @@ public class Position {
 	}
 	
 	public boolean inPlayArea() {
+		
+		// Note that the boundary is not in the play area
+		if(this.latitude >= 55.946233 || this.latitude <= 55.942617) {
+			return false;
+		}
+		
+		if(this.longitude >= -3.184319 || this.longitude <= -3.192473) {
+			return false;
+		}
 		
 		return true;
 	}
