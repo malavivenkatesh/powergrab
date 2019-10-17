c0eeb75ad70d62d59a161e869bd352de72024e48
Malavika Venkatesh <malavi1999@gmail.com>
2019-10-18 00:10:22 +0100

    Start on CLI from main class App.java

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,13 +1,44 @@
 package uk.ac.ed.inf.powergrab;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import com.mapbox.geojson.Feature;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	// Error for argument parsing
+        if (args.length < 7) {
+        	System.out.println("Not enough arguments.");
+        	System.out.println("Aruments needed:");
+        	System.out.println("DD MM YYYY latitude longitude randomseed state");
+        	return;
+        }
+        
+        String year = args[0];
+        String month = args[1];
+        String day = args[2];
+        double latitude = Double.parseDouble(args[3]);
+        double longitude = Double.parseDouble(args[4]);
+        int seed = Integer.parseInt(args[5]);
+        String state = args[6];
+        
+        List<Feature> featureList = Map.getFeatures(year, month, day);
+        Position startPos = new Position(latitude, longitude);
+        
+        Drone drone;
+        
+        if (state.equals("stateless")) {
+        	drone = new StatelessDrone(startPos, seed);
+        }
+        else if (state.equals("stateful")) {
+        	drone = new StatefulDrone(startPos, seed);
+        }
+        else {
+        	System.out.print("Invalid state for drone.");
+        	return;
+        }
+        
+        
     }
 }
